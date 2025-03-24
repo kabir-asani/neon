1ac4d9aac877a7f6177a5598341d1bf8d3f42f2d
Kabir Asani <iamkabirasani@gmail.com>
2025-03-24 16:53:19 +0530

    feat: add user signup endpoint with error handling and JWT token generation

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { hono } from "./routes";
 import { serve } from "@hono/node-server";
 
 serve(hono, (info) => {
-  console.log(`Server is running on port ${info.port}`);
+  console.log(`Server is running @ http://localhost:${info.port}`);
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,42 @@
 import { Hono } from "hono";
+import { signUpWithUsernameAndPassword } from "../controllers/authentication";
+import { SignUpWithUsernameAndPasswordError } from "../controllers/authentication/+types";
 
 export const hono = new Hono();
+
+hono.post("/authentication/sign-up", async (context) => {
+  const { username, password } = await context.req.json();
+
+  try {
+    const result = await signUpWithUsernameAndPassword({
+      username,
+      password,
+    });
+
+    return context.json(
+      {
+        data: result,
+      },
+      201
+    );
+  } catch (e) {
+    if (e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
+      return context.json(
+        {
+          message: "Username already exists",
+        },
+        409
+      );
+    }
+
+    return context.json(
+      {
+        mesage: "Unknown",
+      },
+      500
+    );
+  }
+});
 
 hono.get("/health", (context) => {
   return context.json(
