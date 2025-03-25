035e74f6630573e61dcf569532a613a387c4f5e4
Kabir Asani <iamkabirasani@gmail.com>
2025-03-25 10:49:20 +0530

    Implement authentication routes for user sign-up and log-in

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,86 +1,11 @@
 import { Hono } from "hono";
-import {
-  logInWithUsernameAndPassword,
-  signUpWithUsernameAndPassword,
-} from "../controllers/authentication/authentication-controller";
-import {
-  LogInWtihUsernameAndPasswordError,
-  SignUpWithUsernameAndPasswordError,
-} from "../controllers/authentication/authentication-types";
+import { authenticationRoutes } from "./authentication-routes";
 
-export const hono = new Hono();
+export const allRoutes = new Hono();
 
-hono.post("/authentication/sign-up", async (context) => {
-  const { username, password } = await context.req.json();
+allRoutes.route("/authentication", authenticationRoutes);
 
-  try {
-    const result = await signUpWithUsernameAndPassword({
-      username,
-      password,
-    });
-
-    return context.json(
-      {
-        data: result,
-      },
-      201
-    );
-  } catch (e) {
-    if (e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
-      return context.json(
-        {
-          message: "Username already exists",
-        },
-        409
-      );
-    }
-
-    return context.json(
-      {
-        mesage: "Unknown",
-      },
-      500
-    );
-  }
-});
-
-hono.post("/authentication/log-in", async (context) => {
-  try {
-    const { username, password } = await context.req.json();
-
-    const result = await logInWithUsernameAndPassword({
-      username,
-      password,
-    });
-
-    return context.json(
-      {
-        data: result,
-      },
-      201
-    );
-  } catch (e) {
-    console.log("Error", e);
-
-    if (e === LogInWtihUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD) {
-      return context.json(
-        {
-          message: "Incorrect username or password",
-        },
-        401
-      );
-    }
-
-    return context.json(
-      {
-        message: "Unknown",
-      },
-      500
-    );
-  }
-});
-
-hono.get("/health", (context) => {
+allRoutes.get("/health", (context) => {
   return context.json(
     {
       message: "All Ok",
