eb60b8588da29e2841330841c6c3079a060e5f11
Kabir Asani <iamkabirasani@gmail.com>
2025-03-25 15:26:27 +0530

    Add user retrieval functionality with error handling

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,26 +1,40 @@
 import { Hono } from "hono";
 import { prismaClient } from "../extras/prisma";
 import { tokenMiddleware } from "./middlewares/token-middleware";
+import { getMe } from "../controllers/users/users-controller";
+import { GetMeError } from "../controllers/users/users-types";
 
 export const usersRoutes = new Hono();
 
 usersRoutes.get("/me", tokenMiddleware, async (context) => {
   const userId = context.get("userId");
 
-  const user = await prismaClient.user.findUnique({
-    where: {
-      id: userId,
-    },
-  });
+  try {
+    const user = await getMe({
+      userId,
+    });
 
-  if (user) {
-    return context.json(user, 200);
-  } else {
     return context.json(
       {
-        message: "Missing /me User",
+        data: user,
       },
-      404
+      200
+    );
+  } catch (e) {
+    if (e === GetMeError.BAD_REQUEST) {
+      return context.json(
+        {
+          error: "User not found",
+        },
+        400
+      );
+    }
+
+    return context.json(
+      {
+        message: "Internal Server Error",
+      },
+      500
     );
   }
 });
